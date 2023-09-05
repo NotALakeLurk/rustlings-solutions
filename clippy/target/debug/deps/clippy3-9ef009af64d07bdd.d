924ce25cf2577470a1e0e6080d5bb76831e1b3b7
John Douglas Reed (NotALakeLurk) <TheRedCoat787@Gmail.com>
2023-09-05 18:48:50 -0400

    Finished

---
+++ clippy/target/debug/deps/clippy3-9ef009af64d07bdd.d
@@ -0,0 +1,8 @@
+/home/john/rustlings/exercises/clippy/target/debug/deps/clippy3-9ef009af64d07bdd.rmeta: clippy3.rs Cargo.toml
+
+/home/john/rustlings/exercises/clippy/target/debug/deps/clippy3-9ef009af64d07bdd.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
